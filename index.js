9344b413c0b6460348621244ba62a56083a71f46
Daniel Zhang <danielzhang@Daniels-MacBook-Pro.local>
2020-05-02 15:09:43 -0700

    index.js finished

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let res = 0;
+  for (let i of array){
+    if (Array.isArray(i)){
+      res += sumItems(i);
+    }else{
+      res += i;
+    }
+  }
+  return res;
 }
 
 module.exports = sumItems;
